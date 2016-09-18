435985814907cc9773259f5f6d4794914fd8ffe0
unknown <you@example.com>
2016-09-18 15:17:19 +0800

    finish

--- src/About.js
+++ src/About.js
@@ -1,34 +1,71 @@
 import React, { PropTypes } from 'react';
-import axios from 'axios';
 
+import{ searchGit } from './utils/helpers.js';
 class About extends React.Component {
   constructor(){
     super();
     this.state={
       data:{},
-      wait:true
+      wait:true,
+      inputValue:'user name'
     }
   }
-  componentDidMount(){
-    axios.get('https://api.github.com/users/newming')
-      .then((res) => {
-        // console.log(res.data);
-        this.setState({
-          data:res.data,
-          wait:false
-        })
-      })
-      .catch(function (error) {
-        alert(error);
-      });
-  }
+ // componentDidMount(){
+ //   searchGit()
+ //     .then((data) => {
+ //       this.setState({
+ //         data:data.data,
+ //         wait:false
+ //
+ //       })
+ //     })
+ // }
+ handleInput(e){
+   let value=e.target.value;
+   this.setState({
+     inputValue:value
+   })
+ }
+ handleClick(){
+   let name=this.state.inputValue;
+   searchGit(name )
+       .then((data) => {
+         this.setState({
+           data:data.data,
+           wait:false
+         })
+         console.log(this.state.data)
+       })
+       .catch(function (error) {
+         alert(error);
+       });
+
+ }
   render () {
+    let gitInfo =(
+      <div className="row">
+        <img className="col-xs-3 col-xs-offset-4" src={this.state.data.avatar_url} />
+        <p className="col-xs-3 col-xs-offset-5">{this.state.data.name}</p>
+        <div className="col-xs-6 col-xs-offset-3 row">
+          <div className="col-xs-6">
+            <p>followers</p>
+            <span>{this.state.data.followers}</span>
+          </div>
+          <div className="col-xs-6">
+            <p>following</p>
+            <span>{this.state.data.following}</span>
+          </div>
+        </div>
+      </div>
+  )
     return(
       <div>
-        {
-          this.state.wait ? '正在获取数据' :
-          <img src={this.state.data.avatar_url} />
-        }
+        <h2></h2>
+        <div className="row">
+          <input className="col-xs-4 col-xs-offset-3" type="text" value={this.state.inputValue} onChange={this.handleInput.bind(this)} />
+          <button onClick={this.handleClick.bind(this)} >搜索</button>
+        </div>
+        {this.state.wait ? '正在获取数据' : gitInfo }
       </div>
     )
   }
--- src/Work.js
+++ src/Work.js
@@ -1,22 +1,31 @@
 import React, { PropTypes } from 'react';
 import Card from './components/Card';
 
-import Bg from './image/home1.jpg';
-
-let cardData = [
-  {title:'第一天',desc:'第一天描述',img:Bg,url:'a'},
-  {title:'第二天',desc:'第二天描述',img:Bg,url:'b'},
-  {title:'第三天',desc:'第三天描述',img:Bg,url:'c'},
-  {title:'第四天',desc:'第四天描述',img:Bg,url:'d'},
-  {title:'第五天',desc:'第五天描述',img:Bg,url:'e'}
-]
+import { getJson } from './utils/helpers.js';
 
 class Work extends React.Component {
+  constructor(){
+    super();
+    this.state={
+      cardData:[],
+      wait:true
+    }
+  }
+  componentDidMount(){
+    getJson()
+      .then( (rec) => {
+        this.setState({
+          cardData:rec.getJson,
+          wait:false
+        })
+      })
+  }
   render () {
+    let cards = this.state.cardData.map( (item,i) => <Card {...item} key={i} />)
     return(
       <div className="container-fluid">
         <div className="row" style={{marginTop:'20px'}}>
-          {cardData.map( (item,i) => <Card {...item} key={i} /> )}
+          { this.state.wait ? '请耐心等待片刻' : cards}
         </div>
       </div>
     )
