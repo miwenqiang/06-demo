89cc472ee30e0b484de59fd66f985f47e0d2d41f
unknown <you@example.com>
2016-09-19 17:08:40 +0800

    last

--- src/index.js
+++ src/index.js
@@ -5,6 +5,5 @@ import Routers from './routes';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import './main.css';
 import 'highlight.js/styles/tomorrow-night-bright.css'
-import Marked from './Marked';
 ReactDOM.render(<Routers />, document.getElementById('app'));
 // ReactDOM.render(<Marked />, document.getElementById('app'));
